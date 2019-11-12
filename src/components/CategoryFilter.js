e51b24914aa39a6eacb32029d8450f770af8e09c
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-11-12 13:35:23 +0300

    Implement handleFilterChange function

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,15 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CATEGORIES from '../utils/categories';
 
-const CategoryFilter = ({ changeFilter }) => (
-  <select id="" onChange={(e) => changeFilter(e.target.value)}>
-    {['All'].concat(CATEGORIES).map((category) => (
-      <option key={category} value={category}>
-        {category}
-      </option>
-    ))}
-  </select>
-);
+const CategoryFilter = ({ changeFilter }) => {
+  const handleFilterChange = (e) => {
+    const { value } = e.target;
+    changeFilter(value);
+  };
+
+  return (
+    <select onChange={handleFilterChange}>
+      {['All'].concat(CATEGORIES).map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 CategoryFilter.propTypes = {
   changeFilter: PropTypes.func.isRequired,
